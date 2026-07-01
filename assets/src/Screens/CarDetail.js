29488e6d77584d0f12d994aadae5f4d46c816457
Usaid ur Rehman <usaidurrehman800@gmail.com>
2026-07-01 10:56:46 +0500

    screen files created and navigation in main also created app.json

---
+++ assets/src/Screens/CarDetail.js
@@ -0,0 +1,9 @@
+import react from "react";
+import { View, Text, StyleSheet } from "react-native";
+export default function CarDetail() {
+    return (
+        <View>
+            <Text>Car Detail</Text>
+        </View>
+    )
+}
